43bafc648c863777fc2ebaf99c7db547b3183605
Alex Barabash <a-abarabash@expedia.com>
2016-01-30 11:22:38 -0800

    day2_completed

--- src/test/java/hooks/TestHooks.java
+++ src/test/java/hooks/TestHooks.java
@@ -1,31 +1,10 @@
 package hooks;
 
-import cucumber.api.Scenario;
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
-import io.appium.java_client.AppiumDriver;
 import support.TestBase;
-import utils.CommonUtils;
 
 public class
 TestHooks extends TestBase{
 
 
-    @Before
-    public void startDirver(Scenario scenario) throws Exception {
-        if (driver == null) {
-            CommonUtils.setIOSCapabilities();
-            driver = CommonUtils.getIOSDriver();
-        }
-    }
 
-    @After
-    public void stoptDirver(Scenario scenario) throws Exception {
-        System.out.println("Stopping Appium driver.....");
-        driver.resetApp();
-    }
-
-    public static AppiumDriver getDriver(){
-        return driver;
-    }
 }
--- src/test/java/step_definitions/LoginSteps.java
+++ src/test/java/step_definitions/LoginSteps.java
@@ -1,12 +1,5 @@
 package step_definitions;
 
-import cucumber.api.PendingException;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import org.openqa.selenium.By;
-import org.testng.Assert;
 import support.TestBase;
 
 /**
@@ -14,35 +7,5 @@ import support.TestBase;
  */
 public class LoginSteps extends TestBase {
 
-    @When("^I tap on Login button$")
-    public void iTapOnLoginButton(){
-        driver.findElement(By.name("login_btn")).click();
-    }
 
-    @Then("^I type \"([^\"]*)\" into username field$")
-    public void iTypeIntoUsernameField(String userName){
-        driver.findElement(By.name("user_name")).sendKeys(userName);
-    }
-
-    @And("^I type \"([^\"]*)\" into password field$")
-    public void iTypeIntoPasswordField(String password){
-        driver.findElement(By.name("password")).sendKeys(password);
-    }
-
-    @And("^I verify user is logged in$")
-    public void iVerifyUserIsLoggedIn(){
-        System.out.println("User has been successfully logged in ");
-        Boolean isLogged = driver.findElement(By.name("user_list")).isEnabled();
-        Assert.assertTrue(isLogged);
-    }
-
-    @Then("^I tap on Signup button$")
-    public void iTapOnSignupButton(){
-        System.out.println("Signup button is tapped");
-    }
-
-    @And("^I verify that new user has been created$")
-    public void iVerifyThatNewUserHasBeenCreated(){
-        System.out.println("New user has been created!");
-    }
 }
