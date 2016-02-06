530e4262353eac7e457de896335ff8dde9a5e074
Alex Barabash <a-abarabash@expedia.com>
2016-02-06 10:57:47 -0800

    Login Scenario steps

--- src/test/java/step_definitions/LoginSignupSteps.java
+++ src/test/java/step_definitions/LoginSignupSteps.java
@@ -1,7 +1,10 @@
 package step_definitions;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import support.TestBase;
 
@@ -22,4 +25,20 @@ public class LoginSignupSteps extends TestBase {
     }
 
 
+    @Then("^I type \"([^\"]*)\" into username field$")
+    public void iTypeIntoUsernameField(String name)  {
+        driver.findElement(By.name("user_name")).sendKeys(name);
+
+    }
+
+    @And("^I type \"([^\"]*)\" into password field$")
+    public void iTypeIntoPasswordField(String password)  {
+        driver.findElement(By.name("password")).sendKeys(password);
+    }
+
+    @And("^I verify user is logged in$")
+    public void iVerifyUserIsLoggedIn()  {
+       Boolean result = driver.findElement(By.name("user_list")).isDisplayed();
+       Assert.assertTrue(result);
+    }
 }
